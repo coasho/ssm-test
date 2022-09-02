9f0da0698829235cb321e0e22763baf4043b1469
waveshare <waveshare@gmail.com>
2022-09-02 23:09:47 +0800

    1.1.1

--- src/main/java/com/waveshare/controller/StudentController.java
+++ src/main/java/com/waveshare/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.waveshare.controller;
 
 import com.waveshare.bean.Student;
 import com.waveshare.service.StudentService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
